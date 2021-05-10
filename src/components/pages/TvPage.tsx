62c570e93043795c33d5414ec5d5639c8f00d8ec
ROnic404 <ronic@inbox.ru>
2021-05-10 13:17:47 +0400

    add custom scrollbar

--- src/components/pages/TvPage.tsx
+++ src/components/pages/TvPage.tsx
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import tvSchedule from '../../store/tvSchedule';
 import colors from '../../styles/constants';
 
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+
 const DivTvPage = styled.div`
   margin: 2.5rem auto;
 `;
@@ -12,8 +15,6 @@ const ChannelsList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 1.6rem;
-  max-height: 650px;
-  overflow-y: scroll;
 `;
 
 const ChannelsItem = styled.li`
@@ -62,29 +63,47 @@ const ProgramTime = styled.p`
 const ProgramName = styled.p`
 `;
 
+const CustomScroll = styled(SimpleBar)`
+  max-height: 650px;
+  padding-right: 2rem;
+  .simplebar-track {
+    background: ${colors.gray_6};
+    width: 8px;
+    border-radius: 2px;
+  }
+  .simplebar-scrollbar::before {
+    background-color: ${colors.gray_4};
+    width: 4px;
+    height: 500px;
+    border-radius: 2px;
+  }
+`;
+
 const TvPage: FC = () => {
   return (
     <DivTvPage className="container">
-      <ChannelsList>
-        {tvSchedule.map(channel => (
-          <ChannelsItem key={channel.id}>
-            <DivChannelLogo>
-              <ChannelLogo src={channel.img} alt={channel.title} />
-            </DivChannelLogo>
-            <ChannelDescription>
-              <ChannelTitle>{channel.title}</ChannelTitle>
-              <GuideList>
-                {channel.guide.map(program => (
-                  <DivProgram key={program.id} active={program.active}>
-                    <ProgramTime>{program.time}</ProgramTime>
-                    <ProgramName>{program.name}</ProgramName>
-                  </DivProgram>
-                ))}
-              </GuideList>
-            </ChannelDescription>
-          </ChannelsItem>
-        ))}
-      </ChannelsList>
+      <CustomScroll>
+        <ChannelsList>
+          {tvSchedule.map(channel => (
+            <ChannelsItem key={channel.id}>
+              <DivChannelLogo>
+                <ChannelLogo src={channel.img} alt={channel.title} />
+              </DivChannelLogo>
+              <ChannelDescription>
+                <ChannelTitle>{channel.title}</ChannelTitle>
+                <GuideList>
+                  {channel.guide.map(program => (
+                    <DivProgram key={program.id} active={program.active}>
+                      <ProgramTime>{program.time}</ProgramTime>
+                      <ProgramName>{program.name}</ProgramName>
+                    </DivProgram>
+                  ))}
+                </GuideList>
+              </ChannelDescription>
+            </ChannelsItem>
+          ))}
+        </ChannelsList>
+      </CustomScroll>
     </DivTvPage>
   );
 }
